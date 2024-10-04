49afbfadb8400f0cfba94323884b07b4dbab57dd
hurdledatrade <160125597+hurdledatrade@users.noreply.github.com>
2024-10-04 04:30:41 -0500

    Add files via upload
    helloworld2.py

---
+++ helloworld2.py
@@ -0,0 +1,9 @@
+# This script takes an input from the user and prints "HelloWorld" followed by that input
+
+def main():
+    user_input = input("Please enter something: ")
+    print(f"Hello{user_input}")
+
+
+if __name__ == "__main__":
+    main()
